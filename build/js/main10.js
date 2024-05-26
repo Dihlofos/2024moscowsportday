4952dd1a7f6efeb20735e857c4c2e1dc975c66ca
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-05-26 12:19:25 +0400

    initial commit

---
+++ build/js/main10.js
@@ -0,0 +1,96 @@
+"use strict";
+(function () {
+  let upButton = document.querySelector(".up");
+
+  if (upButton) {
+    window.onscroll = function () {
+      if (window.pageYOffset > 260) {
+        upButton.classList.add("up--shown");
+      } else {
+        upButton.classList.remove("up--shown");
+      }
+    };
+  }
+})();
+
+"use strict";
+(function () {
+  const dropdowns = document.querySelectorAll('.js-dropdown');
+
+  dropdowns.forEach((dropdown)=> {
+    const trigger = dropdown.querySelector('.js-dropdown-trigger');
+
+    trigger.addEventListener('click', () => {
+      dropdown.classList.toggle('open');
+    })
+  })
+
+})();
+
+"use strict";
+(function () {
+  const togglers = document.querySelectorAll(".js-faq-toggler");
+  if (!togglers.length) return;
+
+  togglers.forEach((toggler) => {
+    toggler.addEventListener("click", (event) => {
+      const target = event.currentTarget;
+      if (!target) return;
+      const content = target.nextElementSibling;
+      if (!content) return;
+      target.classList.toggle("active");
+      content.classList.toggle("active");
+    });
+  });
+})();
+
+"use strict";
+(function () {
+  setVh();
+
+  window.addEventListener("resize", setVh);
+
+  function setVh() {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+})();
+
+"use strict";
+(function () {
+  const nav = document.querySelector('.js-nav');
+  const toggler = nav.querySelector('.js-nav-toggler');
+  const closeButton = nav.querySelector('.js-nav-close');
+  const links = nav.querySelectorAll('.js-scroll');
+
+  toggler.addEventListener('click', () => {
+    nav.classList.toggle('is-active');
+  })
+
+  closeButton.addEventListener('click', () => {
+    closeNav();
+  })
+
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      closeNav();
+    })
+  })
+
+
+  function closeNav() {
+    nav.classList.remove('is-active');
+  }
+
+
+})();
+
+"use strict";
+(function () {
+  window.scroll = new SmoothScroll(".js-scroll", {
+    speed: 800,
+    speedAsDuration: true,
+    easing: "easeOutQuad",
+  });
+})();
---
+++ source/js/main/main.js
@@ -0,0 +1,12 @@
+"use strict";
+(function () {
+  setVh();
+
+  window.addEventListener("resize", setVh);
+
+  function setVh() {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+})();
